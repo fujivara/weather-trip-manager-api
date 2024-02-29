262a1738d5cbd692ddfa70ad20ba3fbb83fd6d92
fujivara <marat.skyba@gmail.com>
2024-02-29 14:12:09 +0200

    Feature: Added configuration for deployment

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,6 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as process from 'process';
-import * as cookieParser from 'cookie-parser';
 import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap () {
@@ -12,7 +11,6 @@ async function bootstrap () {
       whitelist: true,
     }),
   );
-  app.use(cookieParser());
   await app.listen(process.env.PORT || 4455);
 }
 bootstrap();
